96114bcb798f5528cc6d755a47e60ce13f7e0c06
Nathan Ramos, CFA® <86393277+nathanramoscfa@users.noreply.github.com>
2023-04-10 23:42:48 -0500

    Update run_main.bat

--- run_main.bat
+++ run_main.bat
@@ -1,15 +1,26 @@
 @echo off
 rem Run Python script in a given conda environment from a batch file.
 
+rem User-defined variables:
+rem ---------------------------------------------------------------
 rem Define here the path to your conda installation.
 set CONDA_PATH=C:\Users\25del\anaconda3
+
+rem Define here the path to your etradebot root directory.
+set ETRADEBOT_PATH=G:\My Drive\etradebot
 
 rem Define here the name of your conda environment.
 set CONDA_ENV=etradebot
 
 rem Set ETradeBot to either preview or live trading mode.
-set PREVIEW=False
+rem SETTING THIS TO FALSE WILL PLACE LIVE TRADES! PROCEED WITH CAUTION!
+set PREVIEW=True
 
+rem End of user-defined variables.
+rem ---------------------------------------------------------------
+
+rem Do not modify these commands unless you know what you are doing:
+rem ---------------------------------------------------------------
 rem Activate the conda environment.
 call %CONDA_PATH%\Scripts\activate.bat %CONDA_ENV%
 
@@ -25,3 +36,4 @@ taskkill /IM python.exe /F
 
 rem Deactivate the conda environment.
 call conda deactivate
+rem ---------------------------------------------------------------
