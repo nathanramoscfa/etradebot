fc68e59a1ccdf258640fc174de410de5ffd3015d
Nathan Ramos, CFA® <86393277+nathanramoscfa@users.noreply.github.com>
2023-04-12 21:00:56 -0500

    Update README.md

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='etradebot',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/nathanramoscfa/etradebot',
     license='MIT',
     author='Nathan Ramos, CFA®',
